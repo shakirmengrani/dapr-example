325827596529160d71fbbe977f95c62fce91260d
Shakir Mengrani <shakir.mengrani@snyder.tech>
2022-02-02 05:11:31 +0500

    add zipkin

--- apps/process/src/app/app.service.ts
+++ apps/process/src/app/app.service.ts
@@ -6,11 +6,15 @@ export class AppService {
     private readonly httpService: HttpService
   ){}
   async getData() {
+    try{
     await this.httpService.get("http://localhost:3500/api", {
         headers: {
           "dapr-app-id": "api"
         }
       }).subscribe((value) => console.log(value.data))
+    }catch(err){
+      console.log("error", err)
+    }
       return {message: ""}
   }
 }
